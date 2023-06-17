ffedc30a557207be13e36a27f4564f80c50d1130
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2023-06-17 11:51:37 +0100

    footer is done

---
+++ client/modern-blog/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Footer = () => {
+  return (
+    <footer className="fixed-bottom text-center bg-dark">
+     
+      <h4 className="text-center text-light p-2 mt-1">
+        ModerBlog 2023
+      </h4>
+    </footer>
+  );
+};
+
+export default Footer;
